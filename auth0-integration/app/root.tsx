c4304dbb59e30802ca46ddfd0c26a2c6066834d7
Chris Bartling <chris.bartling@cleanchoiceenergy.com>
2022-04-14 12:30:16 -0500

    Added Tailwind CSS to the mix

--- auth0-integration/app/root.tsx
+++ auth0-integration/app/root.tsx
@@ -7,12 +7,17 @@ import {
   Scripts,
   ScrollRestoration,
 } from '@remix-run/react';
+import styles from '~/../styles/app.css';
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
   title: 'New Remix App',
   viewport: 'width=device-width,initial-scale=1',
 });
+
+export function links() {
+  return [{ rel: 'stylesheet', href: styles }];
+}
 
 // export const loader: LoaderFunction = async () => {
 //   console.info('Running the root loader function');

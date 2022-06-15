4af568e2008a09552db286b48f8542ac7f9de809
Chris Bartling <chris.bartling@cleanchoiceenergy.com>
2022-06-15 11:51:29 -0500

    feat: logout functionality

--- auth0-integration/app/routes/login.tsx
+++ auth0-integration/app/routes/login.tsx
@@ -1,9 +1,5 @@
-import { Form } from '@remix-run/react';
+import UnauthenticatedHeader from '~/components/UnauthenticatedHeader';
 
 export default function Login() {
-  return (
-    <Form action="/auth/auth0" method="post" className="p-2 ">
-      <button className="border-2">Login with Auth0</button>
-    </Form>
-  );
+  return <UnauthenticatedHeader />;
 }

0da357f6b79e4b656c23f4e70e2eab5395e84156
Chris Bartling <chris.bartling@cleanchoiceenergy.com>
2022-06-16 12:55:35 -0500

    feat: more components

--- auth0-integration/app/routes/dashboard.tsx
+++ auth0-integration/app/routes/dashboard.tsx
@@ -1,11 +1,13 @@
 import AuthenticatedHeader from '~/components/AuthenticatedHeader';
 import HeaderBanner from '~/components/HeaderBanner';
+import Modal from '~/components/Modal';
 
 export default function Dashboard() {
   return (
     <>
       <HeaderBanner />
       <AuthenticatedHeader />
+      <Modal />
     </>
   );
 }

082eaf9cc9e3eb3fc9dabf07e315e60d87fb08f8
Christopher Bartling <chris.bartling@gmail.com>
2022-10-21 23:36:11 -0500

    fix: double logout

--- auth0-integration/app/routes/logout.tsx
+++ auth0-integration/app/routes/logout.tsx
@@ -3,6 +3,6 @@ import { authenticator } from '~/services/auth.server';
 
 export const loader: LoaderFunction = ({ request }) => {
   return authenticator.logout(request, {
-    redirectTo: '/login',
+    redirectTo: `https://${process.env.AUTH0_DOMAIN}/v2/logout?client_id=${process.env.AUTH0_CLIENT_ID}&returnTo=${process.env.BASE_URL}/login`,
   });
 };

33d759d87a90e2a7d6f693523c3d2fd5170262b0
Christopher Bartling <103122029+bartling-cce@users.noreply.github.com>
2022-11-14 10:35:56 -0600

    feat: implemented HTTPS

--- secure-http/server.js
+++ secure-http/server.js
@@ -2,7 +2,9 @@ const path = require("path");
 const express = require("express");
 const compression = require("compression");
 const morgan = require("morgan");
-const { createRequestHandler } = require("@remix-run/express");
+const {createRequestHandler} = require("@remix-run/express");
+const {createServer} = require("https");
+const {readFileSync} = require("fs");
 
 const BUILD_DIR = path.join(process.cwd(), "build");
 
@@ -16,12 +18,12 @@ app.disable("x-powered-by");
 // Remix fingerprints its assets so we can cache forever.
 app.use(
   "/build",
-  express.static("public/build", { immutable: true, maxAge: "1y" })
+  express.static("public/build", {immutable: true, maxAge: "1y"})
 );
 
 // Everything else (like favicon.ico) is cached for an hour. You may want to be
 // more aggressive with this caching.
-app.use(express.static("public", { maxAge: "1h" }));
+app.use(express.static("public", {maxAge: "1h"}));
 
 app.use(morgan("tiny"));
 
@@ -29,21 +31,26 @@ app.all(
   "*",
   process.env.NODE_ENV === "development"
     ? (req, res, next) => {
-        purgeRequireCache();
+      purgeRequireCache();
 
-        return createRequestHandler({
-          build: require(BUILD_DIR),
-          mode: process.env.NODE_ENV,
-        })(req, res, next);
-      }
-    : createRequestHandler({
+      return createRequestHandler({
         build: require(BUILD_DIR),
         mode: process.env.NODE_ENV,
-      })
+      })(req, res, next);
+    }
+    : createRequestHandler({
+      build: require(BUILD_DIR),
+      mode: process.env.NODE_ENV,
+    })
 );
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const httpsOptions = {
+  key: readFileSync(`${process.cwd()}/localhost.key`),
+  cert: readFileSync(`${process.cwd()}/localhost.crt`)
+};
+
+createServer(httpsOptions, app).listen(port, () => {
   console.log(`Express server listening on port ${port}`);
 });
 
